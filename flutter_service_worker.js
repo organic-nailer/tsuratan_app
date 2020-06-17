6e53375f52e675875a2eec562a1aad4f21a1ff10
fastriver <hykwyuk0125@gmail.com>
2020-06-18 01:22:36 +0900

    1.1.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,17 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "8b6ae594365bf0316566b8a610068989",
-"/assets\FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\image\torofi_gold.png": "d6c1638c31b578e3cf1d8f342d37e019",
-"/assets\image\torofi_gray.png": "8f3ba8540231526e7aa69b19af2ab9da",
-"/assets\image\twitter_logo.png": "d73d21cfc1df81e923d62e220e790022",
-"/assets\LICENSE": "02179851bfb2ad33a7a3811fed7ef975",
-"/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/assets\sound\explosion.mp3": "ce38a00a24d7a628c2690bb0de977bf1",
-"/index.html": "982fe212833c645c6911c6279edabc2b",
-"/main.dart.js": "c2543432b6a002d1d95fe5e462ba75ab"
+  "assets/AssetManifest.json": "8b6ae594365bf0316566b8a610068989",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/image/torofi_gold.png": "d6c1638c31b578e3cf1d8f342d37e019",
+"assets/image/torofi_gray.png": "8f3ba8540231526e7aa69b19af2ab9da",
+"assets/image/twitter_logo.png": "d73d21cfc1df81e923d62e220e790022",
+"assets/LICENSE": "b8d0774952b71ba8cf02b145d2630fe1",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/sound/explosion.mp3": "ce38a00a24d7a628c2690bb0de977bf1",
+"index.html": "982fe212833c645c6911c6279edabc2b",
+"/": "982fe212833c645c6911c6279edabc2b",
+"main.dart.js": "6de5f2a70c9b1abec9dc962f773358bd"
 };
 
 self.addEventListener('activate', function (event) {
@@ -33,9 +34,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
